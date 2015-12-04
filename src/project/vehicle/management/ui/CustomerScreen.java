cd2b54075ed7086212f23eaca252227ac5a1ffef
Ying <zhangwei9046@gmail.com>
2015-12-03 21:09:40 -0800

    1st modified...

--- src/project/vehicle/management/ui/CustomerScreen.java
+++ src/project/vehicle/management/ui/CustomerScreen.java
@@ -2,6 +2,7 @@ package project.vehicle.management.ui;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
+import java.awt.Toolkit;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -11,38 +12,41 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class CustomerScreen extends JFrame {
+	//Components that Nan added
 	private JButton searchButton;
 	private JTextField searchTextField;
 	private JLabel searchLabel;
 	private JLabel sortLabel;
 	private JComboBox sortComboBox;
 	
-	public CustomerScreen() {
-		setTitle("Customer Screen");
-		create();
-		add();
-		addListeners();
-		setVisible(true);
-		setSize(1000,1000);
-	}
-	
+	//Components that Nan added
 	
 	public static void main(String[] args) {
 		new CustomerScreen();
-
 	}
+	
+	public CustomerScreen() {
+		initSearchPane();
+		
+		setTitle("Customer Screen");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(Toolkit.getDefaultToolkit().getScreenSize().width,
+				Toolkit.getDefaultToolkit().getScreenSize().width);
+		setVisible(true);
+	}
+	
+	
 
-	private void create() {
+
+	private void initSearchPane() {
+		//create
 		searchButton = new JButton("Search");
 		searchTextField = new JTextField(20);
 		searchLabel = new JLabel("Search:");
 		sortLabel = new JLabel("Sort By:");
 		String[] sortStrings = {"Price (Low to High)", "Price (High to Low)", "Miles (Low to High)", "Miles (High to Low)"};
 		sortComboBox = new JComboBox(sortStrings);
-		
-	}
-
-	private void add() {
+		//add
 		BorderLayout bl = new BorderLayout();
 		Container con = super.getContentPane();
 		con.setLayout(bl);
@@ -53,12 +57,6 @@ public class CustomerScreen extends JFrame {
 		searchAndSortPanel.add(sortLabel);
 		searchAndSortPanel.add(sortComboBox);
 		con.add("North", searchAndSortPanel);
-		
-	}
-
-	private void addListeners() {
-		// TODO Auto-generated method stub
-		
 	}
 
 }
