18da79902d66dbd5fbd5095592241b24926bd084
Jian Hou <Jian Hou@Wolf>
2015-12-03 22:00:44 -0800

    DearlerMainScreen

---
+++ src/project/vehicle/management/ui/DearlerMainScreen.java
@@ -0,0 +1,30 @@
+package project.vehicle.management.ui;
+
+import lecture11.NEUFrame;
+
+public class DearlerMainScreen extends NEUFrame {
+
+	@Override
+	public void create() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void add() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addListeners() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
