d34b984c5ea84ea5080d56ddbe3d5b16dd4f0ba4
Jian Hou <Jian Hou@Wolf>
2015-12-03 22:13:23 -0800

    dealer

--- src/project/vehicle/management/ui/DearlerMainScreen.java
+++ src/project/vehicle/management/ui/DearlerMainScreen.java
@@ -6,24 +6,20 @@ public class DearlerMainScreen extends NEUFrame {
 
 	@Override
 	public void create() {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void add() {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void addListeners() {
-		// TODO Auto-generated method stub
 
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
