8e7ec494eaaa7234f948ead84ca6f1e2c889b1eb
yangneu2015 <liu.yang14@husky.neu.edu>
2015-12-09 20:44:41 -0800

    sortcriteria

--- src/project/vehicle/management/data/SortCriteria.java
+++ src/project/vehicle/management/data/SortCriteria.java
@@ -1,5 +1,14 @@
 package project.vehicle.management.data;
 
 public class SortCriteria {
-
+	private String attribute;
+	private boolean sequence;
+	
+	public SortCriteria(String a, boolean s){
+		attribute = a;
+		sequence = s;
+	}
+	
+	public String getAttribute(){return attribute;}
+	public boolean getSequence(){return sequence;}
 }
