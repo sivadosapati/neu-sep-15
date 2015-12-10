6e90ff3a1145df0c681081a72faef32e595580c8
jessiemakexuan <ma.kex@husky.neu.edu>
2015-12-10 11:39:15 -0800

    update

--- src/project/vehicle/management/ui/SpecificCarScreen.java
+++ src/project/vehicle/management/ui/SpecificCarScreen.java
@@ -1,10 +1,16 @@
 package project.vehicle.management.ui;
 
 import java.awt.GridLayout;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 import javax.swing.border.EmptyBorder;
+
+import project.vehicle.management.data.Car;
+import project.vehicle.management.data.Category;
+import project.vehicle.management.data.Dealer;
+
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
@@ -16,79 +22,71 @@ public class SpecificCarScreen extends JFrame {
 	 */
 	private static final long serialVersionUID = 1L;
 	private JButton search;
-	private JButton backhome;
-	private JLabel carName;
-	private JLabel carPrice;
-	private JLabel date;
-	private JLabel category;
-	private JLabel style;
-	private JLabel color;
-	private JLabel make;
-	private JLabel model;
-	private JLabel year;
-	private JLabel trim;
+	private JLabel carPrice,category,type,make,model,year,trim;
 	private JPanel contentPane;
-	private JLabel label;
-	private JLabel lblSnapshot;
-	private JLabel picture;
-	private JLabel picture1;
-	private JLabel info;
-
-	/**
-	 * Launch the application.
+	private JLabel lblSnapshot,picture,picture1,info;
+	private Car car;
+	private Dealer dealer;
+	/*
+	 * hard code
 	 */
 	public static void main(String[] args) {
-		new SpecificCarScreen();
+		Integer test=2013;
+		Category ca=Category.NEW;
+		Car c = new Car("id","dealerid",ca, test, "BMW",
+	            "Encore", "Convenience", "SUV", 11123.12f);
+		Dealer d=new Dealer();
+		new SpecificCarScreen(c,d);
 	}
-
-	/**
-	 * Create the frame.
+	/*
+	 * hard code
 	 */
-	public SpecificCarScreen() {
+	
+	public SpecificCarScreen(Car car,Dealer dealer) {
+		this.car=car;
+		this.dealer=dealer;
 		create();
-		add();
+		addTopPanel();
+		addPhotoPanel();
+		addCarInfoPanel(); 
+		addDetailPanel();
 		addListeners();
 		display();
+		
 	}
 
 	private void display() {
-		setSize(450, 300);
+		setSize(900, 600);
 		setVisible(true);
 		
 	}
 
 	private void addListeners() {
-		// back to home
 		//back to search
 	}
 
-	private void add() {
+	private void addTopPanel() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(200, 200, 900, 600);
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setBorder(new EmptyBorder(10, 10, 10, 10));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+	
 		
-		backhome = new JButton("Home");
-		backhome.setBounds(2, 6, 62, 23);
-		contentPane.add(backhome);
-		
-		search = new JButton("Search");
-		search.setBounds(69, 6, 74, 23);
+		search = new JButton("Back to Search");
+		search.setBounds(10, 9, 140, 35);
 		contentPane.add(search);
 		
-		label = new JLabel(">");
-		label.setBounds(59, 8, 10, 16);
-		contentPane.add(label);
-		
+
 		lblSnapshot = new JLabel("> Snapshot");
-		lblSnapshot.setBounds(139, 8, 74, 16);
+		lblSnapshot.setBounds(160, 12, 111, 24);
 		contentPane.add(lblSnapshot);
-		
+	}
+	
+	
+	private void addPhotoPanel() {
 		// set photo
-//		JLabel picture = new JLabel(); 
-//		picture.setIcon(new ImageIcon("http://forum.csdn.net/PointForum/ui/scripts/csdn/Plugin/003/monkey/16.gif"));
 		picture = new JLabel();
         int h = this.getHeight();
         int w = this.getWidth();
@@ -99,35 +97,35 @@ public class SpecificCarScreen extends JFrame {
                         + "' src="
                         + "http://i01.i.aliimg.com/wsphoto/v0/798703829/Toy-car-automobile-race-2-alloy-car-models-large-die-model.jpg"
                         + "'></img></body></html>");
-		picture.setBounds(2, 41, 211, 125);
+		picture.setBounds(4, 82, 422, 250);
 		contentPane.add(picture);
-		
-		
+	}
+	
+	private void addCarInfoPanel() {
 		JPanel center = new JPanel();
-		center.setBounds(240, 41, 158, 117);
+		center.setBounds(480, 82, 316, 234);
 		contentPane.add(center);
 		center.setLayout(new BoxLayout(center, BoxLayout.Y_AXIS));
-		center.add(Box.createVerticalStrut(15));
-		center.add(carName);
-		center.add(Box.createVerticalStrut(15));
+		center.add(Box.createVerticalStrut(30));
+		center.add(type);
+		center.add(Box.createVerticalStrut(30));
 		center.add(carPrice);
-		center.add(Box.createVerticalStrut(15));
-		center.add(date);
-		
+		center.add(Box.createVerticalStrut(30));
+		center.add(year);
+	}
+	
+	private void addDetailPanel() {
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-		tabbedPane.setBounds(2, 178, 448, 94);
+		tabbedPane.setBounds(4, 356, 896, 188);
 		contentPane.add(tabbedPane);
 		
 		JPanel features = new JPanel();
 		tabbedPane.addTab("Features", null, features, null);
-		GridLayout gl = new GridLayout(3,3);
+		GridLayout gl = new GridLayout(2,2);
 		features.setLayout(gl);
 		features.add(category);
-		features.add(style);
-		features.add(color);
 		features.add(make);
 		features.add(model);
-		features.add(year);
 		features.add(trim);
 		
 		JPanel photo = new JPanel();
@@ -143,29 +141,26 @@ public class SpecificCarScreen extends JFrame {
                         + "' src="
                         + "https://i.ytimg.com/vi/aBDHBJ-yQWc/maxresdefault.jpg"
                         + "'></img></body></html>");
-		picture1.setBounds(2, 2, 2, 2);
+		picture1.setBounds(4, 4, 4, 4);
 		photo.add(picture1);
 		tabbedPane.addTab("Photos", null, photo, null);
 		
 		
 		JPanel contact = new JPanel();
 		tabbedPane.addTab("Dealers ", null, contact, null);
-		info = new JLabel("Dealers Infomation:");
+		info = new JLabel("Dealers Infomation:");/*+dealer.getName());*/
 		contact.add(info);
 		
 	}
 
 	private void create() {
-		carName = new JLabel("Car name:");
-		carPrice = new JLabel("Car price:");
-		date= new JLabel("Manufacturing date:");
-		category= new JLabel("Category:");
-		style= new JLabel("Body style:");
-		color= new JLabel("Exterior color:");
-		make= new JLabel("Make:");
-		model= new JLabel("Model:");
-		year= new JLabel("Year:");
-		trim= new JLabel("Trim:");
+		carPrice = new JLabel("Car price:"+car.getPrice());
+		category= new JLabel("Category:"+car.getCategory());
+		type= new JLabel("Body style:"+car.getType());
+		make= new JLabel("Make:"+car.getMake());
+		model= new JLabel("Model:"+car.getModel());
+		year= new JLabel("Year:"+car.getYear());
+		trim= new JLabel("Trim:"+car.getTrim());
 		
 	}
 }
