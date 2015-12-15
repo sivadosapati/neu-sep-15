3379c7b2f3523ee1595f3d737d1e27a3bb7fcfbe
Yihong_NEU <yhongtian@gmail.com>
2015-12-15 12:26:38 -0800

    update.................

--- src/project/vehicle/management/ui/DealerUpdate.java
+++ src/project/vehicle/management/ui/DealerUpdate.java
@@ -20,82 +20,81 @@ import javax.swing.table.AbstractTableModel;
 
 import project.vehicle.management.data.Car;
 import project.vehicle.management.data.access.CarManager;
-import project.vehicle.management.data.access.CarManagerImpl; 
 import project.vehicle.management.ui.DearlerMainScreen.MyTableModel;
 
 public class DealerUpdate extends JFrame {
 	
-	public static final int DEFAULT_WIDTH = 1000;
-	public static final int DEFAULT_HEIGHT = 500;
+	public static final int DEFAULT_WIDTH = 1200;
+	public static final int DEFAULT_HEIGHT = 800;
 	static Font font = new Font("Times New Roman", Font.BOLD, 18);
 	private JButton cancelButton;
 	private JButton submitButton;
 	private JPanel panel1;
 	private JPanel panel2;
-	List<Integer> origin, updated;
-	String DealerID;
-	CarManager update;
-	List<Integer> operatedList;
 	private MyTableModel t;
-	/////////////////////////////
+	private JFrame frame;
+	List<Integer> origin, updated;
+	List<Integer> operatedList;
+	List<Car> cars;
+	List<Car> operatedCars;
+	CarManager update;
+	
+
 	public void addTable(){
 		panel1 = new JPanel();
 		GridLayout lay = new GridLayout();
 		panel1.setLayout(lay);
+		MyTable mytable = new MyTable(getCarList(operatedList));
+		JTable table = new JTable(mytable);
 //		panel1.setOpaque(true);
-		JTable table = new JTable(t);/
-        table.setPreferredScrollableViewportSize(new Dimension(500, 100));
-        table.setFillsViewportHeight(true);
-        JScrollPane scrollPane = new JScrollPane(table);
-        panel1.add(scrollPane);
+        	table.setPreferredScrollableViewportSize(new Dimension(500, 100));
+        	table.setFillsViewportHeight(true);
+        	JScrollPane scrollPane = new JScrollPane(table);
+        	panel1.add(scrollPane);
 	}
 	
+	private List<Car> getCarList(List<Integer> carIndex) {
+		operatedCars = new ArrayList<>();
+		List<Car> ori = t.getCars();
+		for(int i = 0; i<carIndex.size();i++){
+			operatedCars.add(ori.get(carIndex.get(i)));
+		}
+		return operatedCars;
+	}
+
 	public void addButton(){
 		cancelButton = new JButton("CANCEL");
-        submitButton = new JButton("SUBMIT");
-        cancelButton.setFont(font);
-        submitButton.setFont(font);
-        panel2 = new JPanel();
-        FlowLayout out = new FlowLayout();
-        panel2.setLayout(out);
-        panel2.add(submitButton);
-        panel2.add(cancelButton);
+        	submitButton = new JButton("SUBMIT");
+        	cancelButton.setFont(font);
+        	submitButton.setFont(font);
+        	panel2 = new JPanel();
+        	FlowLayout out = new FlowLayout();
+        	panel2.setLayout(out);
+        	panel2.add(submitButton);
+        	panel2.add(cancelButton);
 	}
 	
 	public  void display(){
-	JFrame frame = new JFrame("UPDATE");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        BorderLayout out = new BorderLayout();
-        frame.setLayout(out);
-        frame.add(panel1,"Center");
-        frame.add(panel2, "South"); 
-        frame.setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
-        frame.setLocation(850,400);
-        frame.setVisible(true);
+		frame = new JFrame("UPDATE");
+        	BorderLayout out = new BorderLayout();
+        	frame.setLayout(out);
+        	frame.add(panel1,"Center");
+        	frame.add(panel2, "South"); 
+        	frame.setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+        	frame.setLocation(850,400);
+        	frame.setVisible(true);
 	}
 	
 	public DealerUpdate(CarManager cm, List<Integer> list, MyTableModel mtm) throws IOException {
-        //DearlerMainScreen dms = new DearlerMainScreen("gmps-chaparral2");
 		this.t = mtm;
-	    this.operatedList = list;
-	    this.update = cm;
+	    	this.operatedList = list;
+	    	this.update = cm;
 		addButton();
-        addTable(); 
-        display();
-        addListeners();
+        	addTable(); 
+        	display();
+        	addListeners();
        
-        
-    }
-	
-	public static void main(String[] args){
-		try {  
-//			DearlerMainScreen dms = new DearlerMainScreen("gmps-chaparral2");
-			new DealerUpdate(null, null, null);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
+    	}
 
 	public void addListeners() {
 		ButtonClick bc = new ButtonClick();
@@ -109,26 +108,106 @@ public class DealerUpdate extends JFrame {
 		public void actionPerformed(ActionEvent e){
 			if(e.getSource() == submitButton){
 				try {
-					update(operatedList);
+					update(operatedCars);
 				} catch (IOException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
-				System.exit(0);
+				frame.dispose();
 			}
 			if(e.getSource() == cancelButton){
-				//operatedList = origin;
-				System.exit(0);
+				operatedList = origin;
+				frame.dispose();
 			}
 		}
 	}
 	
+	class MyTable extends AbstractTableModel {
+		private String[] Items = {"ID","DealerID","Category", "Year", "Make","Model", "Trim", "Type", "Price"};
+		private List<Car> cars = null;
+		
+		//copy data from dealer main
+		public List<Integer> copy(List<Integer> operatedList){
+			origin = new ArrayList<Integer>();
+			for(int i = 0; i < operatedList.size(); i++){
+				origin.add(operatedList.get(i));
+			}
+			return origin;
+		}
+		
+		public MyTable(List<Car> cars) {
+			super();
+			this.cars = cars;
+		}
+		
+		public int getColumnCount() {
+        		 return Items.length;
+        }
+
+        public int getRowCount() {
+            return cars.size();
+        }
+
+        public String getColumnName(int col) {
+            return Items[col];
+        }
+
+        public Object getValueAt(int row, int col) {
+            Car oneCar = getCars().get(row);
+        	switch(col){
+        	case 0:
+        		return oneCar.getID();
+        	case 1:
+        		return oneCar.getDealerID();
+        	case 2:
+        		return oneCar.getCategory();
+        	case 3:
+        		return oneCar.getYear();
+        	case 4:
+        		return oneCar.getMake();
+        	case 5:
+        		return oneCar.getModel();
+        	case 6:
+        		return oneCar.getTrim();
+        	case 7:
+        		return oneCar.getType();
+        	case 8:
+        		return oneCar.getPrice();
+        	default:
+        		return null;
+        	}
+        }
+
+        public List<Car> getCars() {
+		return cars;
+	}
+
+
+	public Class getColumnClass(int c) {
+            return getValueAt(0, c).getClass();
+        }
+        
+        public boolean isCellEditable(int row, int col) {
+            if (col > 1) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        
+        public void setValueAt(Object value, int row, int col) {
+        	if(true){
+        		System.out.println("Setting value at " + row + "," + col + " to " + value);
+            	fireTableCellUpdated(row, col);
+            }
+        }
+		
+}
 	
 	
-	
-	private void update(List<Integer> operatedList) throws IOException {
-		for (Integer index : operatedList) {
-			;//update.updateCar(t.cars.get(index));
+	private void update(List<Car> updatedCarList) throws IOException {		
+		for (int i = 0 ; i<updatedCarList.size(); i++) {
+			update.updateCar(updatedCarList.get(i));
 		}
 	}
 }
