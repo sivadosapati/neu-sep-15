0fbb65aeae53aa34b33a3ab1250980feb8eaf20c
Ying <zhangwei9046@gmail.com>
2015-12-14 19:18:33 -0800

    Changes

--- src/project/vehicle/management/data/access/DealerCarManagerImpl.java
+++ src/project/vehicle/management/data/access/DealerCarManagerImpl.java
@@ -3,11 +3,13 @@ package project.vehicle.management.data.access;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.swing.JOptionPane;
+
+import project.vehicle.management.ui.MainScreen;
 
 public class DealerCarManagerImpl implements DealerCarManager {
 	private File file;
