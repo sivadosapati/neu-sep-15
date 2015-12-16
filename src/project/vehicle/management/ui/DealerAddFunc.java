42a80570022cb2ecdd2bd0e2ad6b5540bc80867f
Joyce Xin Ding <ding.xin@husky.neu.edu>
2015-12-16 15:34:19 -0800

    This is the final change! I'm serious...

--- src/project/vehicle/management/ui/DealerAddFunc.java
+++ src/project/vehicle/management/ui/DealerAddFunc.java
@@ -35,7 +35,7 @@ public class DealerAddFunc extends JFrame {
 	GridBagConstraints c = new GridBagConstraints();
 
 	DealerAddFunc(CarManagerImpl dealerid, MyTableModel mtm) {
-		setTitle("Add Function");
+		setTitle("Add");
 		setSize(590, 470);
 		setLayout(g);
 		
@@ -67,117 +67,117 @@ public class DealerAddFunc extends JFrame {
 		add(jc);
 	}	
 		//Add all components
-		public void addComponent() {
-			new Car();
-			head = new JLabel(new ImageIcon("pictures/DealerScreen2.jpg"));
-			add(g, c, head, 0, 0, 3, c.gridheight);
+	public void addComponent() {
+		new Car();
+		head = new JLabel(new ImageIcon("pictures/DealerScreen2.jpg"));
+		add(g, c, head, 0, 0, 3, c.gridheight);
 			
-			// Title
-			noteInformation = new JLabel("Add A Car!");
-			add(g, c, noteInformation, 0, 1, 1, 1);
+		// Title
+		noteInformation = new JLabel("Add A Car!");
+		add(g, c, noteInformation, 0, 1, 1, 1);
 			
-			// ID
-			ID = new JLabel("ID:");
-			add(g, c, ID, 0, 2, 1, 1);
+		// ID
+		ID = new JLabel("ID:");
+		add(g, c, ID, 0, 2, 1, 1);
 			
-			// Input ID
-			textID = new JTextField(20);
-			add(g, c, textID, 1, 2, 2, 1);
+		// Input ID
+		textID = new JTextField(20);
+		add(g, c, textID, 1, 2, 2, 1);
 
-			// jCategory
-			jCategory = new JLabel("Category:");
-			add(g, c, jCategory, 0, 4, 1, 1);
+		// jCategory
+		jCategory = new JLabel("Category:");
+		add(g, c, jCategory, 0, 4, 1, 1);
 
-			// Choose from the new and old
-			NewType = new JRadioButton("New");
-			OldType = new JRadioButton("Used");
-			CerType = new JRadioButton("Certified");
+		// Choose from the new and old
+		NewType = new JRadioButton("New");
+		OldType = new JRadioButton("Used");
+		CerType = new JRadioButton("Certified");
 			
-			ButtonGroup group = new ButtonGroup();
+		ButtonGroup group = new ButtonGroup();
 			
-			group.add(NewType);
-			group.add(OldType);
-			group.add(CerType);
+		group.add(NewType);
+		group.add(OldType);
+		group.add(CerType);
 			
-			add(g, c, NewType, 1, 4, 1, 1);
-			add(g, c, OldType, 2, 4, 1, 1);
-			add(g, c, CerType, 1, 5, 1, 1);
+		add(g, c, NewType, 1, 4, 1, 1);
+		add(g, c, OldType, 2, 4, 1, 1);
+		add(g, c, CerType, 1, 5, 1, 1);
 			
-			// Year
-			Year = new JLabel("Year:");
-			add(g, c, Year, 0, 6, 1, 1);
+		// Year
+		Year = new JLabel("Year:");
+		add(g, c, Year, 0, 6, 1, 1);
 			
-			// content of year
-			String[] YEARS = new String[16];
-			for (int i = 2000, k = 0; i <= 2015; i++, k++) {
-				YEARS[k] = i + "";
-			}
-			year = new JComboBox(YEARS);
-			add(g, c, year, 1, 6, 1, 1);
+		// content of year
+		String[] YEARS = new String[16];
+		for (int i = 2000, k = 0; i <= 2015; i++, k++) {
+			YEARS[k] = i + "";
+		}
+		year = new JComboBox(YEARS);
+		add(g, c, year, 1, 6, 1, 1);
 			
-			// Make
-			Model = new JLabel("Make:");
-			add(g, c, Model, 0, 7, 1, 1);
+		// Make
+		Model = new JLabel("Make:");
+		add(g, c, Model, 0, 7, 1, 1);
 			
-			// Input Make
-			textMake = new JTextField(20);
-			add(g, c, textMake, 1, 7, 2, 1);
+		// Input Make
+		textMake = new JTextField(20);
+		add(g, c, textMake, 1, 7, 2, 1);
 			
-			// Model
-			Model = new JLabel("Model:");
-			add(g, c, Model, 0, 8, 1, 1);
+		// Model
+		Model = new JLabel("Model:");
+		add(g, c, Model, 0, 8, 1, 1);
 			
-			// Input Model
-			textModel = new JTextField(20);
-			add(g, c, textModel, 1, 8, 2, 1);
+		// Input Model
+		textModel = new JTextField(20);
+		add(g, c, textModel, 1, 8, 2, 1);
 			
-			// Trim
-			Trim = new JLabel("Trim:");
-			add(g, c, Trim, 0, 9, 1, 1);
+		// Trim
+		Trim = new JLabel("Trim:");
+		add(g, c, Trim, 0, 9, 1, 1);
 			
-			// Input Trim
-			textTrim = new JTextField(20);
-			add(g, c, textTrim, 1, 9, 2, 1);
+		// Input Trim
+		textTrim = new JTextField(20);
+		add(g, c, textTrim, 1, 9, 2, 1);
 			
-			// Type
-			Type = new JLabel("Type:");
-			add(g, c, Type, 0, 10, 1, 1);
+		// Type
+		Type = new JLabel("Type:");
+		add(g, c, Type, 0, 10, 1, 1);
 			
-			// Choose from the types
-			Type1 = new JRadioButton("CAR"); 
-			add(g, c, Type1, 1, 10, 1, 1);
-			Type2 = new JRadioButton("TRUCK");
-			add(g, c, Type2, 2, 10, 1, 1);
-			Type3 = new JRadioButton("WAGON");
-			add(g, c, Type3, 1, 12, 1, 1);
-			Type4 = new JRadioButton("VAN");
-			add(g, c, Type4, 1, 11, 1, 1);
-			Type5 = new JRadioButton("SUV");
-			add(g, c, Type5, 2, 11, 1, 1);
+		// Choose from the types
+		Type1 = new JRadioButton("CAR"); 
+		add(g, c, Type1, 1, 10, 1, 1);
+		Type2 = new JRadioButton("TRUCK");
+		add(g, c, Type2, 2, 10, 1, 1);
+		Type3 = new JRadioButton("WAGON");
+		add(g, c, Type3, 1, 12, 1, 1);
+		Type4 = new JRadioButton("VAN");
+		add(g, c, Type4, 1, 11, 1, 1);
+		Type5 = new JRadioButton("SUV");
+		add(g, c, Type5, 2, 11, 1, 1);
 
-			ButtonGroup group1 = new ButtonGroup();
+		ButtonGroup group1 = new ButtonGroup();
 			
-			group1.add(Type1);
-			group1.add(Type2);
-			group1.add(Type3);
-			group1.add(Type4);
-			group1.add(Type5);
+		group1.add(Type1);
+		group1.add(Type2);
+		group1.add(Type3);
+		group1.add(Type4);
+		group1.add(Type5);
 			
-			// Price
-			Price = new JLabel("Price:");
-			add(g, c, Price, 0, 13, 1, 1);
+		// Price
+		Price = new JLabel("Price:");
+		add(g, c, Price, 0, 13, 1, 1);
 			
-			// Input Price
-			textPrice = new JTextField(20);
-			add(g, c, textPrice, 1, 13, 2, 1);
+		// Input Price
+		textPrice = new JTextField(20);
+		add(g, c, textPrice, 1, 13, 2, 1);
 
-			// Submit button
-			submit = new JButton("Submit");
-			add(g, c, submit, 1, 15, 1, 1);
+		// Submit button
+		submit = new JButton("Submit");
+		add(g, c, submit, 1, 15, 1, 1);
 			
-			// Cancel button
-			cancel = new JButton("Cancel");
-			add(g, c, cancel, 2, 15, 1, 1);
+		// Cancel button
+		cancel = new JButton("Cancel");
+		add(g, c, cancel, 2, 15, 1, 1);
 
 		}
 
