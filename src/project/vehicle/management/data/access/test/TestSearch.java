6e50a3d7bdaf54a3fa75ccb232698983bdce2116
Jay <buaajjg@126.com>
2015-12-16 00:51:30 -0800

    Update TestSearch.java

--- src/project/vehicle/management/data/access/test/TestSearch.java
+++ src/project/vehicle/management/data/access/test/TestSearch.java
@@ -29,8 +29,7 @@ public class TestSearch {
         SearchFilter sf2 = new SearchFilter(category, null, null, null, null, null,"Ford");
         List<Car> list2 = test.search(sf2);
         for(int i=0;i<list2.size();i++){
-        	
-        	Assert.assertEquals("Ford", list2.get(i).getMake());
+           	Assert.assertEquals("Ford", list2.get(i).getMake());
         }
         
         
@@ -38,30 +37,23 @@ public class TestSearch {
         SearchFilter sf3 = new SearchFilter(category, null, null, null, r, null,null);
         List<Car> list3 = test.search(sf3);
         for(int i=0;i<list3.size();i++){
-        	
-        	
         	Assert.assertTrue(list3.get(i).getYear().intValue()<=2015);
         	Assert.assertTrue(list3.get(i).getYear().intValue()>=2013);
-        	
         }
         
         Range ra= new Range(5000,15000);
         SearchFilter sf4 = new SearchFilter(category, null, null, null, null, ra,null);
         List<Car> list4 = test.search(sf4);
         for(int i=0;i<list4.size();i++){
-        	
         	Assert.assertTrue(list4.get(i).getPrice()<=15000f);
         	Assert.assertTrue(list4.get(i).getPrice()>=5000f);
-        	
         }
         
         boolean[] category2 = {false, true, false};
         SearchFilter sf5 = new SearchFilter(category2, null, null, null, null, null,null);
         List<Car> list5 = test.search(sf5);
         for(int i=0;i<list5.size();i++){
-        	
         	Assert.assertEquals(list5.get(i).getCategory(),Category.USED);
-        
         }
         
     }
