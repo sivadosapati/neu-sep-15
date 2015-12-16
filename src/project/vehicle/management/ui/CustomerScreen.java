e8526547e4aee45349fcb7bd0cb3aec4cad76123
kristinechiang <christinejyx@gmail.com>
2015-12-15 19:58:36 -0800

    l

--- src/project/vehicle/management/ui/CustomerScreen.java
+++ src/project/vehicle/management/ui/CustomerScreen.java
@@ -46,22 +46,23 @@ public class CustomerScreen extends JFrame {
 	private JLabel searchLabel;
 	private JLabel sortLabel;
 	private JLabel header;
-	private JComboBox sortComboBox,comboBox,comboBox_1,comboBox_2,comboBox_3,comboBox_4;
-	private JCheckBox chckbxNew,chckbxUsed,chckbxCertified;
-	private boolean[] category={false,false,false};
-	
+	private JComboBox sortComboBox, comboBox, comboBox_1, comboBox_2, comboBox_3, comboBox_4;
+	private JCheckBox chckbxNew, chckbxUsed, chckbxCertified;
+	private boolean[] category = { false, false, false };
+
 	private JTable table;
-	private SearchFilter sf = new SearchFilter(null, null, null, null, null, null,null);
+	private SearchFilter sf = new SearchFilter(null, null, null, null, null, null, null);
 	private SortCriteria sc = new SortCriteria(null, true);
-    boolean initcombomodel=false;
-    boolean initcombotrim=false;
+	boolean initcombomodel = false;
+	boolean initcombotrim = false;
 	private CarManager carManager;
 
-	List<String> make,model,trim;
+	List<String> make, model, trim;
+
 	public CustomerScreen() {
 		init();
 	}
-	
+
 	public CustomerScreen(CarManager carManager) {
 		this.carManager = carManager;
 		init();
@@ -75,15 +76,14 @@ public class CustomerScreen extends JFrame {
 			initTablePane(cars);
 		} catch (IOException e) {
 			e.printStackTrace();
-		} //dealerID
-		
+		} // dealerID
+
 		initchoosePane(carManager);
 
 		setTitle("CustomerScreen ——>Dealer: " + ((CarManagerImpl) carManager).getDealerID());
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setLocation(100, 0);
-		setSize(Toolkit.getDefaultToolkit().getScreenSize().width,
-				Toolkit.getDefaultToolkit().getScreenSize().height);
+		setSize(Toolkit.getDefaultToolkit().getScreenSize().width, Toolkit.getDefaultToolkit().getScreenSize().height);
 		setVisible(true);
 		addListeners();
 		getRootPane().setDefaultButton(searchButton);
@@ -96,7 +96,8 @@ public class CustomerScreen extends JFrame {
 		searchTextField = new JTextField(20);
 		searchLabel = new JLabel("Search:");
 		sortLabel = new JLabel("Sort By:");
-		String[] sortStrings = { " ", "Price (Low to High)", "Price (High to Low)", "Year (Low to High)", "Year (High to Low)"};
+		String[] sortStrings = { " ", "Price (Low to High)", "Price (High to Low)", "Year (Low to High)",
+				"Year (High to Low)" };
 		sortComboBox = new JComboBox(sortStrings);
 		header = new JLabel(new ImageIcon("pictures/CustomerScreen.jpg"));
 		// add
@@ -118,7 +119,7 @@ public class CustomerScreen extends JFrame {
 		mainPanel.add(searchAndSortPanel);
 		con.add("North", mainPanel);
 	}
-	
+
 	private void addListeners() {
 		ButtonClick bc = new ButtonClick();
 		searchButton.addActionListener(bc);
@@ -126,17 +127,17 @@ public class CustomerScreen extends JFrame {
 		sortComboBox.addActionListener(ss);
 	}
 
-	class ButtonClick implements ActionListener{
+	class ButtonClick implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent ae) {
-			if (ae.getSource() == searchButton){
+			if (ae.getSource() == searchButton) {
 				sf.setKeywords(searchTextField.getText());
 				refreshTable();
 			}
 		}
 	}
 
-	class SortSelection implements ActionListener{
+	class SortSelection implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent ae) {
 			JComboBox sortComboBox = (JComboBox) ae.getSource();
@@ -144,16 +145,16 @@ public class CustomerScreen extends JFrame {
 			String selectedSort = (String) sortComboBox.getSelectedItem();
 			int i = selectedSort.indexOf(" ");
 			String sortKeyword = selectedSort.substring(0, i);
-			if(selectedSort.contains("High to Low")){
+			if (selectedSort.contains("High to Low")) {
 				highToLow = false;
-			}else{
+			} else {
 				highToLow = true;
 			}
 			sc.setAttribute(sortKeyword);
 			sc.setSequence(highToLow);
-			//System.out.println(sortKeyword);
-			//System.out.println(highToLow);
-			try{
+			// System.out.println(sortKeyword);
+			// System.out.println(highToLow);
+			try {
 				String dealerIDS = ((CarManagerImpl) carManager).getDealerID();
 				CarManagerImpl testAfterSearchSort = new CarManagerImpl(dealerIDS);
 				List<Car> carAfterSearchSort = carManager.sort(sf, sc);
@@ -163,20 +164,18 @@ public class CustomerScreen extends JFrame {
 				e.printStackTrace();
 			}
 		}
-	} 
-	
-    private void initchoosePane(CarManager cm) {
-		
-		make = cm.setMake();
-		
-		
-		JPanel choosecondiPanel=new JPanel();
-		choosecondiPanel.setLayout(new BoxLayout(choosecondiPanel, BoxLayout.Y_AXIS)); 
-		//GridLayout gl=new GridLayout(6,1);
-		//choosecondiPanel.setLayout(gl);
-		
+	}
 
-		JPanel checkboxPanel=new JPanel();
+	private void initchoosePane(CarManager cm) {
+
+		make = cm.setMake();
+
+		JPanel choosecondiPanel = new JPanel();
+		choosecondiPanel.setLayout(new BoxLayout(choosecondiPanel, BoxLayout.Y_AXIS));
+		// GridLayout gl=new GridLayout(6,1);
+		// choosecondiPanel.setLayout(gl);
+
+		JPanel checkboxPanel = new JPanel();
 
 		chckbxNew = new JCheckBox("New");
 		checkboxPanel.add(chckbxNew);
@@ -187,110 +186,106 @@ public class CustomerScreen extends JFrame {
 		chckbxCertified = new JCheckBox("Certified");
 		checkboxPanel.add(chckbxCertified);
 
-		clickCheckAction cna=new clickCheckAction();
+		clickCheckAction cna = new clickCheckAction();
 		chckbxNew.addActionListener(cna);
 		chckbxUsed.addActionListener(cna);
 		chckbxCertified.addActionListener(cna);
 
 		choosecondiPanel.add(checkboxPanel);
 
-		JPanel brandComboPanel=new JPanel();
+		JPanel brandComboPanel = new JPanel();
 		brandComboPanel.setLayout(new BoxLayout(brandComboPanel, BoxLayout.Y_AXIS));
 		JLabel lblBrand = new JLabel("Make");
 		brandComboPanel.add(lblBrand);
-		//lblBrand.setLocation(30, 100);
+		// lblBrand.setLocation(30, 100);
 
 		make.add(0, "");
-		
+
 		comboBox = new JComboBox(make.toArray());
 		comboBox.setSelectedItem(null);
-		//comboBox.setLocation(30, 600);
+		// comboBox.setLocation(30, 600);
 		brandComboPanel.add(comboBox);
 
-		selectAction sa=new selectAction();
+		selectAction sa = new selectAction();
 		comboBox.addActionListener(sa);
 		brandComboPanel.add(lblBrand);
 		brandComboPanel.add(comboBox);
 		choosecondiPanel.add(brandComboPanel);
 
-
-		JPanel modelComboPanel=new JPanel();
+		JPanel modelComboPanel = new JPanel();
 		modelComboPanel.setLayout(new BoxLayout(modelComboPanel, BoxLayout.Y_AXIS));
 		JLabel lblModel = new JLabel("Model");
 		modelComboPanel.add(lblModel);
 
-
 		comboBox_1 = new JComboBox();
-		//comboBox_1.setSelectedItem(null);
+		// comboBox_1.setSelectedItem(null);
 		modelComboPanel.add(comboBox_1);
 		choosecondiPanel.add(modelComboPanel);
 
-		JPanel trimComboPanel=new JPanel();
+		JPanel trimComboPanel = new JPanel();
 		trimComboPanel.setLayout(new BoxLayout(trimComboPanel, BoxLayout.Y_AXIS));
 		JLabel lblTrim = new JLabel("Trim");
 		trimComboPanel.add(lblTrim);
 
 		comboBox_2 = new JComboBox();
-		//comboBox_2.setSelectedItem(null);
+		// comboBox_2.setSelectedItem(null);
 		trimComboPanel.add(comboBox_2);
 		choosecondiPanel.add(trimComboPanel);
-		
 
-
-		JPanel yearComboPanel=new JPanel();
+		JPanel yearComboPanel = new JPanel();
 		yearComboPanel.setLayout(new BoxLayout(yearComboPanel, BoxLayout.Y_AXIS));
 		JLabel lblYear = new JLabel("Year");
 		yearComboPanel.add(lblYear);
 
-		String yearStr[]={"<2000","2000-2005","2006-2010","2011-2015"};
+		String yearStr[] = { "<2000", "2000-2005", "2006-2010", "2011-2015" };
 		comboBox_3 = new JComboBox(yearStr);
 		comboBox_3.setSelectedItem(null);
 		yearComboPanel.add(comboBox_3);
 		choosecondiPanel.add(yearComboPanel);
 
-		JPanel prComboPanel=new JPanel();
+		JPanel prComboPanel = new JPanel();
 		prComboPanel.setLayout(new BoxLayout(prComboPanel, BoxLayout.Y_AXIS));
 		JLabel lblPR = new JLabel("Price");
 		prComboPanel.add(lblPR);
 
-		String priceStr[]={"<10000","10000-15000","15000-20000","20000-25000","25000-30000","30000-50000",">50000"};
+		String priceStr[] = { "<10000", "10000-15000", "15000-20000", "20000-25000", "25000-30000", "30000-50000",
+				">50000" };
 		comboBox_4 = new JComboBox(priceStr);
 		comboBox_4.setSelectedItem(null);
 		prComboPanel.add(comboBox_4);
 		choosecondiPanel.add(prComboPanel);
 
-		selectAction s=new selectAction();
+		selectAction s = new selectAction();
 		comboBox.addActionListener(s);
 		comboBox_1.addActionListener(s);
 		comboBox_2.addActionListener(s);
 		comboBox_3.addActionListener(s);
 		comboBox_4.addActionListener(s);
 
-
-		getContentPane().add("West",choosecondiPanel);
+		getContentPane().add("West", choosecondiPanel);
 
 	}
-    
-    public String checknull(String s){
-    	if(s==""){
-    		
-    		return null;
-    	}else{
-    		return s;
-    	}
-    }
 
-	class clickCheckAction implements ActionListener{ 
+	public String checknull(String s) {
+		if (s == "") {
+
+			return null;
+		} else {
+			return s;
+		}
+	}
+
+	class clickCheckAction implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			JCheckBox cb=(JCheckBox)e.getSource();
-			if(cb==chckbxNew){
-				category[0]=(!category[0]);
-			}else if(cb==chckbxUsed){
-				category[1]=(!category[1]);
-			}else if(cb==chckbxCertified){
-				category[2]=(!category[2]);
+			JCheckBox cb = (JCheckBox) e.getSource();
+			if (cb == chckbxNew) {
+				category[0] = (!category[0]);
+			} else if (cb == chckbxUsed) {
+				category[1] = (!category[1]);
+			} else if (cb == chckbxCertified) {
+				category[2] = (!category[2]);
 			}
 			sf.setCategory(category);
 			refreshTable();
@@ -298,80 +293,79 @@ public class CustomerScreen extends JFrame {
 
 	}
 
-	class selectAction implements ActionListener{
+	class selectAction implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			JComboBox cb=(JComboBox)e.getSource();
+			JComboBox cb = (JComboBox) e.getSource();
 			String choice = cb.getSelectedItem().toString();
-			//System.out.println(choice);
-			Range r,p;
-			
-			
-			List<String> model,trim;
-			if(cb==comboBox){
+			// System.out.println(choice);
+			Range r, p;
+
+			List<String> model, trim;
+			if (cb == comboBox) {
 				sf.setMake(checknull(choice));
-				if(choice==""){
-					//System.out.println("here1");                   
-					sf.setMake(null);		
+				if (choice == "") {
+					// System.out.println("here1");
+					sf.setMake(null);
 				}
 				sf.setModel(null);
 				sf.setTrim(null);
-				model=carManager.setModel(sf.getMake());
+				model = carManager.setModel(sf.getMake());
 				model.add(0, "");
 				DefaultComboBoxModel modelm = new DefaultComboBoxModel(model.toArray());
-				comboBox_1.setModel(modelm);  
-				
-				trim=carManager.setTrim(sf.getModel(), sf.getMake());
+				comboBox_1.setModel(modelm);
+
+				trim = carManager.setTrim(sf.getModel(), sf.getMake());
 				trim.add(0, "");
 				DefaultComboBoxModel trimm = new DefaultComboBoxModel(trim.toArray());
 				comboBox_2.setModel(trimm);
-				
-			}else if(cb==comboBox_1){
+
+			} else if (cb == comboBox_1) {
 				sf.setModel(checknull(choice));
-				if(choice==""){
+				if (choice == "") {
 					sf.setModel(null);
 				}
 				sf.setTrim(null);
-				trim=carManager.setTrim(sf.getModel(), sf.getMake());
+				trim = carManager.setTrim(sf.getModel(), sf.getMake());
 				trim.add(0, "");
 				DefaultComboBoxModel trimm = new DefaultComboBoxModel(trim.toArray());
 				comboBox_2.setModel(trimm);
-			}else if(cb==comboBox_2){
+			} else if (cb == comboBox_2) {
 				sf.setTrim(checknull(choice));
-			}else if(cb==comboBox_3){
-				if (choice=="<2000"){
-					r=new Range(0,2000);					
-				}else if(choice=="2000-2005"){
-					r=new Range(2000,2005);
-				}else if(choice=="2006-2010"){
-					r=new Range(2006,2010);
-				}else{
-					r=new Range(2011,2015);
+			} else if (cb == comboBox_3) {
+				if (choice == "<2000") {
+					r = new Range(0, 2000);
+				} else if (choice == "2000-2005") {
+					r = new Range(2000, 2005);
+				} else if (choice == "2006-2010") {
+					r = new Range(2006, 2010);
+				} else {
+					r = new Range(2011, 2015);
 				}
 				sf.setYear(r);
-			}else{
-				if(choice=="<10000"){
-					p=new Range(0,10000);
-				}else if(choice=="10000-15000"){
-					p=new Range(10000,15000);
-				}else if(choice=="15000-20000"){
-					p=new Range(15000,20000);
-				}else if(choice=="20000-25000"){
-					p=new Range(20000,25000);
-				}else if(choice=="25000-30000"){
-					p=new Range(25000,30000);
-				}else if(choice=="30000-50000"){
-					p=new Range(30000,50000);
-				}else{
-					p=new Range(50000,Integer.MAX_VALUE);
+			} else {
+				if (choice == "<10000") {
+					p = new Range(0, 10000);
+				} else if (choice == "10000-15000") {
+					p = new Range(10000, 15000);
+				} else if (choice == "15000-20000") {
+					p = new Range(15000, 20000);
+				} else if (choice == "20000-25000") {
+					p = new Range(20000, 25000);
+				} else if (choice == "25000-30000") {
+					p = new Range(25000, 30000);
+				} else if (choice == "30000-50000") {
+					p = new Range(30000, 50000);
+				} else {
+					p = new Range(50000, Integer.MAX_VALUE);
 				}
 				sf.setRange(p);
 			}
 			refreshTable();
 		}
 	}
-	
+
 	class CarTableModel extends AbstractTableModel {
 		/**
 		 * 
@@ -379,9 +373,8 @@ public class CustomerScreen extends JFrame {
 		private static final long serialVersionUID = 6660519266094091930L;
 		private List<Car> cars;
 		private List<Object[]> carList;
-		private String[] columnNames = {"Category", "Year", "Brand", 
-				"Model", "Trim", "Price"};
-		
+		private String[] columnNames = { "Category", "Year", "Brand", "Model", "Trim", "Price" };
+
 		public List<Car> getCars() {
 			return cars;
 		}
@@ -400,7 +393,7 @@ public class CustomerScreen extends JFrame {
 				String model = cars.get(i).getModel();
 				String trim = cars.get(i).getTrim();
 				float price = cars.get(i).getPrice();
-				Object[] object = {category, year, make, model, trim, price};
+				Object[] object = { category, year, make, model, trim, price };
 				carList.add(object);
 			}
 		}
@@ -440,7 +433,7 @@ public class CustomerScreen extends JFrame {
 		}
 
 		@Override
-		public Object getValueAt(int rowIndex, int columnIndex) {			
+		public Object getValueAt(int rowIndex, int columnIndex) {
 			return carList.get(rowIndex)[columnIndex];
 		}
 
@@ -464,8 +457,8 @@ public class CustomerScreen extends JFrame {
 
 	private void initTablePane(List<Car> cars) {
 		table = new JTable(new CarTableModel(cars));
-		
-		//Double Click to enter Detail Screen
+
+		// Double Click to enter Detail Screen
 		table.addMouseListener(new MouseAdapter() {
 			public void mouseClicked(MouseEvent e) {
 				List<Car> current_cars = ((CarTableModel) table.getModel()).getCars();
@@ -487,22 +480,18 @@ public class CustomerScreen extends JFrame {
 		CarManagerImpl test;
 		try {
 			test = new CarManagerImpl(((CarManagerImpl) carManager).getDealerID());
-			List<Car> carAfterSearch=test.search(sf);
+			List<Car> carAfterSearch = test.search(sf);
 			table.setModel(new CarTableModel(carAfterSearch));
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
-		} //dealerID
+		} // dealerID
 		table.updateUI();
 	}
 
 	public static void main(String[] args) throws Exception {
-		CarManager carManager = new CarManagerFactory()
-				.getCarManager("gmps-bresee");
+		CarManager carManager = new CarManagerFactory().getCarManager("gmps-bresee");
 		new CustomerScreen(carManager);
-		
+
 	}
 }
-
-
-
