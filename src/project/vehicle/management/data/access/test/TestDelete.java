51420dd0e0bd66538061b547ee16476544570c39
Jay <buaajjg@126.com>
2015-12-15 21:09:46 -0800

    update testDelete

--- src/project/vehicle/management/data/access/test/TestDelete.java
+++ src/project/vehicle/management/data/access/test/TestDelete.java
@@ -9,8 +9,10 @@ public class TestDelete {
 
     @Test
     public void test() throws IOException {
-        CarManagerImpl test = new CarManagerImpl("testdelete&update");
-        test.deleteCar("1212");
+        CarManagerImpl test = new CarManagerImpl("testadd");
+        test.deleteCar("121");//if there is a car with ID '121', it will be deleted. Please check the file 'testadd'
+        test.deleteCar("12123");//there is not a car with ID '12123', the file does not change after deleteCar execute
+        test.deleteCar("98123");//if there is no car in file 'testadd',please execute the test case 'TestAdd' and check the file'testadd'
     }
 
 }
