ab005be543a30e7ae1b8a7acebb2469f8b67c5ec
zhangyang3723 <zhang.yang3@husky.neu.edu>
2015-12-15 20:44:35 -0800

    update getdealerid method with reading template

--- src/project/vehicle/management/data/access/DealerCarManagerImpl.java
+++ src/project/vehicle/management/data/access/DealerCarManagerImpl.java
@@ -1,9 +1,6 @@
 package project.vehicle.management.data.access;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,52 +9,39 @@ import javax.swing.JOptionPane;
 import project.vehicle.management.ui.MainScreen;
 
 public class DealerCarManagerImpl implements DealerCarManager {
-	private File file;
+    private File file;
 
-	public DealerCarManagerImpl() {
-		file = new File("dealers");
-	}
+    public DealerCarManagerImpl() {
+        file = new File("dealers");
+    }
+    
+   /**
+    * Get all dealer ID from file
+    * 
+    * @param none
+    * @return the dealer id array
+    */
+    public String[] getDealerIds() {
+        List<String> dealerIds = new ArrayList<String>();
+        FileReadingTemplate template = new FileReadingTemplate() {
 
-	public String[] getDealerIds() {
-		List<String> dealerIds = new ArrayList<String>();
-		try {
-			FileInputStream fis;
-			fis = new FileInputStream(file);
-			InputStreamReader isr = new InputStreamReader(fis);
-			BufferedReader br = new BufferedReader(isr);
-			String thisLine = null;
-			while ((thisLine = br.readLine()) != null) {
-				String[] arr = thisLine.split("\t");
-				String dealerId = arr[0];
-				dealerIds.add(dealerId);
-			}
-			br.close();
-		}catch (Exception e) {
-			JOptionPane.showMessageDialog(new MainScreen(""), e.getMessage()+"\nPlease contact administrator to fix it!");
-			System.exit(1);
-			e.printStackTrace();
-		}
-		return dealerIds.toArray(new String[dealerIds.size()]);
-	}
+            @Override
+            public void processLine(String line) {
+                String[] arr = line.split("\t");
+                String dealerId = arr[0];
+                dealerIds.add(dealerId);
+            }
 
-	public String[] getDealerIdsOld() {
-		List<String> dealerIds = new ArrayList<String>();
-		FileReadingTemplate template = new FileReadingTemplate() {
-
-			@Override
-			public void processLine(String line) {
-				String[] arr = line.split("\t");
-				String dealerId = arr[0];
-				dealerIds.add(dealerId);
-
-			}
-
-		};
-		template.setSkipFirstLine(false);
-		try {
-			template.parseFile(file);
-		} catch (Exception e) {
-		}
-		return dealerIds.toArray(new String[dealerIds.size()]);
-	}
+        };
+        template.setSkipFirstLine(false);
+        try {
+            template.parseFile(file);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(new MainScreen(""),
+                    e.getMessage() + "\nPlease contact administrator to fix it!");
+            System.exit(1);
+            e.printStackTrace();
+        }
+        return dealerIds.toArray(new String[dealerIds.size()]);
+    }
 }
--- src/project/vehicle/management/data/access/test/TestGetDealerIDs.java
+++ src/project/vehicle/management/data/access/test/TestGetDealerIDs.java
@@ -11,15 +11,14 @@ public class TestGetDealerIDs {
     public void test() {
         DealerCarManagerImpl dcm = new DealerCarManagerImpl();
         String[] ids = dcm.getDealerIds();
-        Assert.assertEquals("gmps-priority",ids[0]);
-        Assert.assertEquals("gmps-davis-chevrolet",ids[1]);
-        Assert.assertEquals("gmps-gray-stroudsburg",ids[12]);
-        Assert.assertEquals("gmps-mcgee",ids[6]);
-        Assert.assertEquals("gmps-blue-ribbon",ids[16]);
-        Assert.assertEquals("gmps-goldstein",ids[21]);
-        Assert.assertEquals("gmps-mckenna",ids[22]);
-        Assert.assertEquals("gmps-curry",ids[25]);
-     
+        Assert.assertEquals("gmps-priority", ids[0]);
+        Assert.assertEquals("gmps-davis-chevrolet", ids[1]);
+        Assert.assertEquals("gmps-gray-stroudsburg", ids[12]);
+        Assert.assertEquals("gmps-mcgee", ids[6]);
+        Assert.assertEquals("gmps-blue-ribbon", ids[16]);
+        Assert.assertEquals("gmps-goldstein", ids[21]);
+        Assert.assertEquals("gmps-mckenna", ids[22]);
+        Assert.assertEquals("gmps-curry", ids[25]);
     }
-
+    
 }
