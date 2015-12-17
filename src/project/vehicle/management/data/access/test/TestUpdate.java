adb4ec3f7745ef8637b5118cfcef6916641e38a6
Jay <buaajjg@126.com>
2015-12-16 16:54:55 -0800

    Update TestUpdate.java

--- src/project/vehicle/management/data/access/test/TestUpdate.java
+++ src/project/vehicle/management/data/access/test/TestUpdate.java
@@ -11,9 +11,9 @@ public class TestUpdate {
 
     @Test
     public void test() throws IOException {
-        CarManagerImpl test = new CarManagerImpl("testdelete&update");
-        Car car = new Car("121", "121", Category.CERTIFIED, 2015, "BMW",
-                "BMW", "None", "SUV", 2f);
+        CarManagerImpl test = new CarManagerImpl("testadd");
+        Car car = new Car("98771", "312312", Category.CERTIFIED, 2015, "BMW",
+                "BMW", "None", "SUV", 200000f);
         test.updateCar(car);
     }
 
